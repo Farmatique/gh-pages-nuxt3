ed92f1ab75aa5c7e547caff7e60d48044385dd22
Yevhen <ymarynych@mirantis.com>
2025-01-23 14:42:35 +0200

    init nuxt3 files

---
+++ components/Card.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <h2>{{ card.title }}</h2>
+        <div>
+            <p>{{ card.body }}</p>
+        </div>
+    </div>
+</template>
+
+<script setup>
+const {card} = defineProps({
+    card: {type: Object, required: true}
+})
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <h1>Hello nuxt3-gh-pages!</h1>
+    <Card v-for="(card, index) in cards" :key="index" :card="card"/>
+</template>
+
+<script setup>
+    import Card from '~/components/Card'
+
+    const cards = ref([
+        {title: 'Card1 title', body: 'Card 1 body'},
+        {title: 'Card2 title', body: 'Card 2 body'}
+    ])
+</script>  
